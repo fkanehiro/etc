65614a0344a84273b84cc4193da90a54b1a2223c
Fumio KANEHIRO <fkanehiro@gmail.com>
2012-06-19 12:29:21 +0900

    integrats openhrp3_upload.sh into Makefile

--- debian/openhrp3_upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-\cp ./debian/control* ./debian/changelog openhrp-3.1.3/debian/
-#cd openhrp-3.1.3; ../ppa_publish.sh ppa:hrg/release lucid maverick natty oneiric precise
-cd openhrp-3.1.3; ../ppa_publish.sh ppa:hrg/release precise
-
-
-
-
