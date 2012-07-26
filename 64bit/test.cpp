875123e26c091acdbab2dd0209155d6e37381d53
Fumio KANEHIRO <f-kanehiro@aist.go.jp>
2012-07-26 19:13:48 +0900

    fixes a bug

--- 64bit/test.cpp
+++ 64bit/test.cpp
@@ -3,5 +3,5 @@
 int main(int argc, char *argv[])
 {
     std::cout << "sizeof(int) = " << sizeof(int) << std::endl;
-    std::cout << "sizeof(long int) = " << sizeof(int) << std::endl; 
+    std::cout << "sizeof(long) = " << sizeof(long) << std::endl; 
 }
