a22ffc55e9c7b8d5d5cad2f8296a461a7e1c16c2
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-05-04 15:20:10 +0900

    removes an unused parameter

--- udp/stdin2udp.cpp
+++ udp/stdin2udp.cpp
@@ -13,19 +13,15 @@
 
 main(int argc, char *argv[]) {
   const char *host=DEFAULT_HOSTNAME;
-  int port=DEFAULT_PORT,l=100;
+  int port=DEFAULT_PORT;
 
   for (int i=1; i<argc; i++){
     if (strcmp(argv[i], "-p")==0){
       port = atoi(argv[++i]);
     }else if(strcmp(argv[i], "-h")==0){
       host = argv[++i];
-    }else if(strcmp(argv[i], "-l")==0){
-      l = atoi(argv[++i]);
     }
   }
-  printf("destination = %s:%d\n", host, port);
-  printf("length(-l) = %d\n", l);
 
   int udp_socket;
   udp_socket = socket(AF_INET,SOCK_DGRAM,0);
@@ -43,13 +39,13 @@ main(int argc, char *argv[]) {
   bcopy(dest_host->h_addr,(char *)&dest_addr.sin_addr,dest_host->h_length);
   dest_addr.sin_port = htons(port);
 
-  char buf[1024];
-  fgets(buf, 1024, stdin);
-  printf("buf=[%s]\n", buf);
+#define BUFLEN 1024
+  char buf[BUFLEN];
+  fgets(buf, BUFLEN, stdin);
 
   int ret;
-  if ((ret = sendto(udp_socket,buf,l,0,(sockaddr *)&dest_addr,sizeof(dest_addr)))!=l){
-    printf("return value=%d, data length=%d\n", ret, l); 
+  if ((ret = sendto(udp_socket,buf,BUFLEN,0,(sockaddr *)&dest_addr,sizeof(dest_addr)))!=BUFLEN){
+    printf("return value=%d, data length=%d\n", ret, BUFLEN); 
   };
 
   close(udp_socket);
