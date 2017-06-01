66acf26c85edbe1ece0f15dacda918507f1192a4
Fumio KANEHIRO <fkanehiro@gmail.com>
2017-06-01 11:16:58 +0900

    fix compile errors on ubuntu16

--- qhull/bbox.cpp
+++ qhull/bbox.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 #include <hrpModel/Link.h>
 #include <hrpModel/Body.h>
--- qhull/qhull.cpp
+++ qhull/qhull.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 extern "C" {
 #include <qhull/qhull_a.h>
