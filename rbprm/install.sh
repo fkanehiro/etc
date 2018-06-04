e0d695bfae25abec5dd7748ec6a350f19e78a3fc
Fumio KANEHIRO <fkanehiro@gmail.com>
2018-06-04 18:45:33 +0900

    add rbprm

---
+++ rbprm/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+export DEVEL_DIR=$HOME/src/laas
+
+mkdir -p $DEVEL_DIR
+mkdir -p $DEVEL_DIR/src
+mkdir -p $DEVEL_DIR/install
+
+sudo apt-get install autoconf g++ cmake libboost-dev liburdfdom-dev libassimp-dev ros-indigo-xacro ros-indigo-kdl-parser ros-indigo-common-msgs ros-indigo-tf ros-indigo-tf-conversions ros-indigo-libccd ros-indigo-octomap ros-indigo-resource-retriever ros-indigo-srdfdom ros-indigo-pr2-robot flex bison asciidoc source-highlight git libomniorb4-dev omniorb-nameserver omniidl omniidl-python libltdl-dev python-matplotlib libtinyxml2-dev liblog4cxx10-dev libltdl-dev qt4-dev-tools libqt4-opengl-dev libqtgui4 oxygen-icon-theme libcdd-dev
+
+sudo apt-get build-dep openscenegraph
+
+wget -O $DEVEL_DIR/config.sh https://raw.githubusercontent.com/humanoid-path-planner/hpp-doc/rbprm/doc/config.sh
+wget -O $DEVEL_DIR/src/Makefile https://raw.githubusercontent.com/humanoid-path-planner/hpp-doc/rbprm/doc/Makefile
+
+cd $DEVEL_DIR
+source config.sh
+
+cd src
+make robot_state_chain_publisher.install
+make hpp-model-urdf.install
+make hpp-rbprm-corba.install
+
+git clone https://github.com/iit-DLSLab/hyq-description.git
+git clone https://github.com/iit-DLSLab/sensors-description.git
+
+rosrun xacro xacro.py `rospack find hyq_description`/robots/hyq_model.urdf.xacro -o $DEVEL_DIR/install/share/hpp-rbprm-corba/hyq.urdf
+
